23cb2524ffe23b2d4ec515f1c645160b47e54898
Mehmet ÜLKER <mehmet-ulker@hotmail.com>
2024-09-05 11:18:08 +0300

    Hero completed.

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className="sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -1,5 +1,7 @@
 import logo_img from "./logo-black.png";
+import hero_img from "./hero_img.png";
 
 export const assets = {
   logo_img,
+  hero_img,
 };
